6d70af7137c30b2e2bf369fc4dcc6f4b5d2296ce
unknown <santhosh8857@gmail.com>
2022-04-04 14:21:58 +0530

    added custom button

---
+++ components/PrimaryButton.js
@@ -0,0 +1,51 @@
+import { Text, View, Pressable, StyleSheet } from "react-native";
+
+function PrimaryButton({ children }) {
+  function pressHandler() {
+    console.log("Pressed!");
+  }
+
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={pressHandler}
+        android_ripple={{ color: "#640233" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    backgroundColor: "#72063c",
+    elevation: 2,
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
+
+// To be known
+// pressed destructured from object, if the pressed is true then apply these styles or the other one
+// like hover effect!!
+// rippleeffect will not be applied to IOS instead we can use stylings
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,12 +1,65 @@
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
   return (
-    <View>
-      <TextInput />
-      BUTTONS
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    borderRadius: 8,
+    backgroundColor: "#4e0329",
+
+    // for adding shadow - elevation is only for android
+    elevation: 4,
+
+    // to add shadow to IOS
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    marginVertical: 8,
+    fontSize: 32,
+    fontWeight: "bold",
+    textAlign: "center",
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+  },
+});
